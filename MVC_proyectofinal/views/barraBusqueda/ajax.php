ed7c87e5ce32a6ffc9fe5d32ac3b8e28fede7d45
gerardopm <gerardopimentelpm@gmail.com>
2022-04-25 12:20:28 +0200

    Barra de busqueda funcionando

---
+++ MVC_proyectofinal/views/barraBusqueda/ajax.php
@@ -0,0 +1,82 @@
+<?php
+
+//Including Database configuration file.
+
+//Database connection.
+
+$con = MySQLi_connect(
+
+   "localhost", //Server host name.
+
+   "root", //Database username.
+
+   "", //Database password.
+
+   "sgec" //Database name or anything you would like to call it.
+
+);
+
+
+
+//Check connection
+
+if (MySQLi_connect_errno()) {
+
+   echo "Failed to connect to MySQL: " . MySQLi_connect_error();
+
+}
+//Getting value of "search" variable from "script.js".
+
+if (isset($_POST['search'])) {
+
+//Search box value assigning to $Name variable.
+
+   $Name = $_POST['search'];
+
+//Search query.
+
+   $Query = "SELECT id FROM aulas WHERE id LIKE '%$Name%' AND habilitado = 1 LIMIT 6";
+
+//Query execution
+
+   $ExecQuery = MySQLi_query($con, $Query);
+
+//Creating unordered list to display result.
+
+   echo '
+
+<div>
+
+   ';
+
+   //Fetching result from database.
+
+   while ($Result = MySQLi_fetch_array($ExecQuery)) {
+
+       ?>
+
+   <!-- Creating unordered list items.
+
+        Calling javascript function named as "fill" found in "script.js" file.
+
+        By passing fetched result as parameter. -->
+
+   <a href="?c=calendario&id=<?php echo $Result['id']; ?>" >
+
+   <!-- Assigning searched result in "Search box" in "search.php" file. -->
+   <div>
+       <?php echo $Result['id']; ?>
+       
+
+   </div></a>
+
+   <!-- Below php code is just for closing parenthesis. Don't be confused. -->
+
+   <?php
+
+}}
+
+
+?>
+
+</div>
---
+++ MVC_proyectofinal/views/barraBusqueda/barra.php
@@ -0,0 +1,90 @@
+
+
+   <!-- Including our scripting file. -->
+
+   <script type="text/javascript">
+      //Getting value from "ajax.php".
+
+
+ $(document).ready(function() {
+ 
+ //On pressing a key on "Search box" in "search.php" file. This function will be called.
+
+ $("#search").keyup(function() {
+
+     //Assigning search box value to javascript variable named as "name".
+
+     var name = $('#search').val();
+
+     //Validating, if "name" is empty.
+
+     if (name == "") {
+
+         //Assigning empty value to "display" div in "search.php" file.
+
+         $("#displaySearch").html("");
+
+     }
+
+     //If name is not empty.
+
+     else {
+
+         //AJAX is called.
+
+         $.ajax({
+
+             //AJAX type is "Post".
+
+             type: "POST",
+
+             //Data will be sent to "ajax.php".
+
+             url: "views/barraBusqueda/ajax.php",
+
+             //Data, that will be sent to "ajax.php".
+
+             data: {
+
+                 //Assigning value of "name" into "search" variable.
+
+                 search: name
+
+             },
+
+             //If result found, this funtion will be called.
+
+             success: function(html) {
+
+                 //Assigning result to "display" div in "search.php" file.
+
+                 $("#displaySearch").html(html).show();
+
+             }
+
+         });
+
+     }
+
+ });
+
+});
+   </script>
+
+
+
+<!-- Search box. -->
+
+   <input type="text" id="search" placeholder="Buscar por nombre..." />
+
+   <br>
+
+   
+
+   <br />
+
+   <!-- Suggestions will be displayed in below div. -->
+
+   <div id="displaySearch"></div>
+
+
