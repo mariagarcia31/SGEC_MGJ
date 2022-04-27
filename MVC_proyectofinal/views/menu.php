a663f7927eb095b260ae9c695361127f7f550242
[mariagvalero3] <mariagvalero3@gmail.com>
2022-04-27 13:38:26 +0200

    logo

--- MVC_proyectofinal/views/menu.php
+++ MVC_proyectofinal/views/menu.php
@@ -300,7 +300,8 @@ echo '  <a class="btn-menu" href="?c=ayuda&page=1"><i class="bi bi-info-circle">
 <span class="glyphicon glyphicon-align-justify"></span>
 </span>
 <div class="cabecera-todo">
-    <a class="logoEnlace" href="https://www.educa2.madrid.org/web/centro.ies.ciudadescolar.madrid" target="_blank"><img class="logo" style="width:135px" src="libs/img/logo_ciudadescolar.png"></a>
+    <a class="logoEnlace" href="https://www.educa2.madrid.org/web/centro.ies.ciudadescolar.madrid" target="_blank"><img class="logo" style="    width: 175px;
+    height: 85px;" src="libs/img/logo_ciudadescolar.png"></a>
     <h3 class="cabecera">Sistema de Gestión de Espacios Comunes</h3>
 </div>
 <li class="nav-item dropdown no-arrow">
